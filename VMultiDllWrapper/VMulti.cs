814748b0a9a0e761fb1f97da508652357d64ae85
Simon Nilsson <sim.nilsson@gmail.com>
2014-02-09 19:28:04 +0100

    Added basic multitouch reporting

--- VMultiDllWrapper/VMulti.cs
+++ VMultiDllWrapper/VMulti.cs
@@ -27,6 +27,9 @@ namespace VMultiDllWrapper
         [DllImport("VMultiDll.dll")]
         public static extern bool vmulti_update_joystick(IntPtr vmulti, ushort buttons, byte hat, byte x, byte y, byte z, byte rz, byte throttle);
 
+        [DllImport("VMultiDll.dll")]
+        public static extern bool vmulti_update_multitouch(IntPtr vmulti, MultitouchPointerInfoRaw[] pTouch, byte actualCount, byte request_type, byte report_control_id);
+
         IntPtr vmulti;
         bool connected;
 
@@ -53,6 +56,18 @@ namespace VMultiDllWrapper
             if (connected)
             {
                 return vmulti_update_joystick(vmulti, report.getButtonsRaw(), report.getPOVRaw(), report.getJoystickXRaw(), report.getJoystickYRaw(), 0, 128, 0);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool updateMultitouch(MultitouchReport report)
+        {
+            if (connected)
+            {
+                return vmulti_update_multitouch(vmulti, report.getTouchesRaw(), report.getTouchesCountRaw(), report.getRequestType(), report.getReportControlId());
             }
             else
             {
