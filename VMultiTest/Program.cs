931766d83884e91037f4bdbebdbccee1058647a2
Simon Nilsson <sim.nilsson@gmail.com>
2014-02-09 19:28:04 +0100

    Added basic multitouch reporting

--- VMultiTest/Program.cs
+++ VMultiTest/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
 using VMultiDllWrapper;
 
 namespace VMultiTest
@@ -14,6 +18,7 @@ namespace VMultiTest
 
             VMulti vmulti = new VMulti();
             Console.WriteLine("Connect: " + vmulti.connect());
+            /* Joystick test
             double i = 0;
             bool running = true;
             while (running)
@@ -36,7 +41,42 @@ namespace VMultiTest
                 
                 System.Threading.Thread.Sleep(100);
             }
+             * */
+
+
+            while(true)
+            {
+                List<MultitouchPointerInfo> touches = new List<MultitouchPointerInfo>();
+                bool spacePressed = Convert.ToBoolean(GetKeyState(0x20) & 0x8000);
+                if (spacePressed)
+                {
+                    Console.WriteLine("yay a pointer");
+
+                    MultitouchPointerInfo pointer = new MultitouchPointerInfo();
+                    Point mousePos = Control.MousePosition;
+                    Console.WriteLine(mousePos);
+                    pointer.X = ((double)mousePos.X)/2560.0;
+                    pointer.Y = ((double)mousePos.Y)/1440.0;
+
+                    Console.WriteLine("X: " + pointer.X);
+                    Console.WriteLine("Y: " + pointer.Y);
+
+                    touches.Add(pointer);
+                }   
+                MultitouchReport report = new MultitouchReport(touches);
+
+                if(!vmulti.updateMultitouch(report))
+                {
+                    Console.WriteLine("fail");
+                }
+
+                System.Threading.Thread.Sleep(10);
+            }
+
             vmulti.disconnect();
         }
+
+        [DllImport("USER32.dll")]
+        static extern short GetKeyState(int nVirtKey);
     }
 }
